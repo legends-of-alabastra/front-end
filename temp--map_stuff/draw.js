f338e4ed7d426110e79eb65becc569299c2966d6
dustinsnoap <afrobacon@gmail.com>
2020-01-21 18:33:51 -0500

    updated render function updated island tileset add some helper functions minor tweaks to various functions

--- temp--map_stuff/draw.js
+++ temp--map_stuff/draw.js
@@ -1,19 +1,8 @@
 const canvas = document.getElementById('background')
 const ctx = canvas.getContext('2d')
 
-const decompress_tile = tile => {
-    if(tile) {
-        return tile.reduce((sum, tq) => {
-            tq += ''
-            if(tq.length < 16) tq = Array(17 - tq.length).join('0') + tq
-            return sum + tq
-        }, '').split('')
-    }}
-
-const draw_map = (data, background_color = '#00f', pixel_size = 10) => {
+const draw_map = (data, background_color = '#00f', pixel_size = 10, colors) => {
     const {tileset, map} = data
-    console.log(tileset)
-    console.log(map)
     const height = map.length * 8 * pixel_size
     const width = map[0].length * 8 * pixel_size
     ctx.fillStyle = background_color
@@ -26,7 +15,7 @@ const draw_map = (data, background_color = '#00f', pixel_size = 10) => {
         row.forEach(col => {
             if(col) {
                 tile = decompress_tile(tileset[col])
-                draw_tile(ctx, coordinates, pixels_per_line, pixel_size, ['#000','#370','#130'], tile)
+                draw_tile(ctx, coordinates, pixel_size, colors, tile)
             }
             coordinates.x += pixels_per_line * pixel_size
         })
@@ -34,17 +23,17 @@ const draw_map = (data, background_color = '#00f', pixel_size = 10) => {
     })
 }
 
-const draw_tile = (ctx, coordinates, pixels_per_line, pixel_size, colors, data) => {
-	for(let row=0; row<pixels_per_line; row++) {
-		for(let col=0; col<pixels_per_line; col++) {
-            const offset_x = col * pixel_size + coordinates.x
-            const offset_y = row * pixel_size + coordinates.y
-			if(data[row*pixels_per_line+col] != 0) {
-				ctx.fillStyle = colors[data[row*pixels_per_line+col]-1] //minus 1 because 0 is assumed to be transparent
-				ctx.fillRect(offset_x, offset_y, pixel_size, pixel_size)
+const draw_tile = (ctx, coordinates, pixel_size, colors, data) => {
+    data.forEach((color_row, row) => {
+        const offset_x = row * pixel_size + coordinates.y
+        color_row.forEach((color, col) => {
+            const offset_y = col * pixel_size + coordinates.x
+            if(color != 0) {
+                ctx.fillStyle = colors[color]
+                ctx.fillRect(offset_y, offset_x, pixel_size, pixel_size)
             }
-        }
-	}
+        })    
+    })
 }
 
-draw_map(generate_map(tileset, universe_variables), '#00f', 1)
+draw_map(generate_map(tileset, universe_variables), '#00f', 1, tileset.island.colors)
