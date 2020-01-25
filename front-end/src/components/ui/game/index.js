2b20edc9443b6a48947c051d56c493aa8747a1d5
dustinsnoap <afrobacon@gmail.com>
2020-01-25 04:05:56 -0500

    map renders only the portion needed

--- front-end/src/components/ui/game/index.js
+++ front-end/src/components/ui/game/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 //style
 import Style from './style'
 //components
+import Map from './map'
 
 export default class Game extends React.Component {
     constructor(props) {
         super(props)
         this.state = {}
     }
+    componentDidMount() {
+
+    }
     render() {
         return (
             <Style className='game'>
-                <span>yerp</span>
+                <Map></Map>
             </Style>
         )
     }
