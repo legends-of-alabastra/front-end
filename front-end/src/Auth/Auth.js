eec50def8f6049d0e31ae55d864d58240a53419f
douglasjordan2 <douglasjordan2@gmail.com>
2020-01-26 17:24:44 -0700

    auth and landing done

---
+++ front-end/src/Auth/Auth.js
@@ -0,0 +1,195 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const logo = require('../assets/Logo-t-back.png')
+
+export default class Auth extends Component {
+  state = {
+    type: 'login',
+    email: '',
+    username: '',
+    password: ''
+  }
+
+  changeType = type => this.setState({ type })
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    const { email, username, password } = this.state;
+
+    if(this.state.type === 'login') {
+      const user = {
+        username, password
+      }
+
+      axios
+        .post('https://alabastraapp.herokuapp.com/api/auth/login', user)
+        .then(res => {
+          localStorage.setItem('token', res.data.token)
+        })
+        .then(() => this.props.history.push('/app'))
+        .catch(err => console.log(err))
+
+    } else {
+      const user = {
+        email, username, password
+      }
+
+      axios
+        .post('https://alabastraapp.herokuapp.com/api/auth/register', user)
+        .then(res => {
+          localStorage.setItem('token', res.data.token)
+        })
+        .then(() => this.props.history.push('/app'))
+        .catch(err => console.log(err))
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.placeholder.toLowerCase()]: e.target.value })
+  }
+
+  render() {
+    const { type } = this.state;
+
+    const styles = {
+      _page: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+        border: '2px solid white',
+        background: '#2F2F2F'
+      },
+  
+      _form: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px',
+        height: '90vh'
+      },
+  
+      _logo: {
+        height: '50vh'
+      },
+  
+      _header: {
+        fontSize: '3rem',
+        color: 'white',
+        fontFamily: "'Pirata One', cursive"
+      },
+  
+      _subheader: {
+        fontSize: '1.3rem',
+        color: 'white',
+      },
+  
+      _input: {
+        background: 'unset',
+        fontSize: '1.1rem',
+        color: 'white',
+        padding: '3px 5px',
+        width: '60%'
+      },
+  
+      _buttons: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '60%'
+      },
+  
+      _button: {
+        width: '45%',
+        height: '25px',
+        background: '#F1DBB1',
+        fontSize: '1.1rem',
+        cursor: 'pointer'
+      }
+    }
+
+    const { _page, _form, _logo, _header, _subheader, _input, _button, _buttons } = styles;
+
+    return (
+      <div style = { _page }>
+        <form 
+          style = { _form }
+          onSubmit = { e => this.handleSubmit(e) }
+        >
+          <img 
+            style = { _logo }
+            src = { logo } 
+          />
+          <h1 style = { _header }>Welcome</h1>
+          <h2 style = { _subheader }>Please Log In or Sign Up</h2>
+          { type === 'login' ? 
+            <>
+              <input 
+                placeholder="Username" 
+                onChange = { this.handleChange }
+                value = { this.state.username }
+                style = { _input }
+              />
+              <input 
+                placeholder="Password"
+                onChange = { this.handleChange }
+                value = { this.state.password }
+                style = { _input }
+              />
+              <div style = { _buttons }>
+                <button 
+                  style = { _button }
+                  onClick = { () => this.changeType('signup') }
+                >
+                  Sign Up
+                </button>
+                <input 
+                  style = { _button }
+                  type="submit"
+                  value="Set Sail!"
+                />
+              </div>
+            </>
+          :
+            <>
+              <input 
+                placeholder="Email" 
+                onChange = { this.handleChange }
+                value = { this.state.email }
+                style = { _input }
+              />
+              <input 
+                placeholder="Username"
+                onChange = { this.handleChange }
+                value = { this.state.username }
+                style = { _input }
+              />
+              <input 
+                placeholder="Password"
+                onChange = { this.handleChange }
+                value = { this.state.password }
+                style = { _input }
+              />
+              <div style = { _buttons }>
+                <button 
+                  style = { _button }
+                  onClick = { () => this.changeType('login') }
+                >
+                  Log In
+                </button>
+                <input 
+                  style = { _button }
+                  type="submit"
+                  value="Set Sail!"
+                />
+              </div>
+            </>
+          }
+        </form>
+      </div>
+    )
+  }
+}
--- front-end/src/components/InfoWindow.js
+++ front-end/src/components/InfoWindow.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import Pusher from "pusher-js";
 
 export default class InfoWindow extends React.Component {
@@ -38,72 +37,74 @@ export default class InfoWindow extends React.Component {
   }
 
   render() {
+    const info_view = {
+      position: 'absolute',
+      width: '500px',
+      height: '238px',
+      right: '0',
+      bottom: '37px',
+      border: '2px solid #ffdc61',
+      background: 'rgba(0, 0, 0, 0.4)',
+      boxSizing: 'border-box',
+      transition: '0.5s'
+    }
+
+    const info_tab = {
+      zIndex: '1000',
+      position: 'absolute',
+      width: '72px',
+      height: '29px',
+      left: '60px',
+      top: '-30px',
+      cursor: 'pointer',
+      background: '#f1dbb1',
+      border: '1px solid #ffdc61',
+      boxSizing: 'border-box',
+      borderRadius: '4px 4px 0px 0px',
+    }
+
+    const info_text = {
+      marginTop: '-1px',
+      fontFamily: 'Pirata One',
+      fontStyle: 'normal',
+      fontWeight: 'normal',
+      fontSize: '24px',
+      lineHeight: '31px',
+      color: '#000000'
+    }
+
+    const info_feed = {
+      overflow: 'auto',
+      height: '235px'
+    }
+
+    const welcome_message = {
+      fontFamily: 'Pirata One',
+      fontStyle: 'normal',
+      fontWeight: 'normal',
+      fontSize: '24px',
+      lineHeight: '31px',
+      color: '#ffffff'
+    }
+
     return (
-      <InfoView id="info-view">
-        <InfoTab onClick={() => this.boxToggle()}>
-          <InfoText>Info</InfoText>
-        </InfoTab>
-        <InfoFeed id="info-feed">
-          <WelcomeMessage>Welcome to Legends of Alabastra!</WelcomeMessage>
-        </InfoFeed>
-      </InfoView>
+      <div 
+        style = { info_view }
+        id="info-view"
+      >
+        <div 
+          style = { info_tab }
+          onClick={() => this.boxToggle()}
+        >
+          <div style = { info_text }>Info</div>
+        </div>
+        <div 
+          id="info-feed"
+          style = { info_feed }
+        >
+          <div style = { welcome_message }>Welcome to Legends of Alabastra!</div>
+        </div>
+      </div>
     );
   }
 }
-
-// Styles
-
-// background: rgba(0, 0, 0, 0.4);
-const InfoView = styled.div`
-  position: absolute;
-  width: 500px;
-  height: 238px;
-  right: 0;
-  bottom: 37px;
-  border: 2px solid #ffdc61;
-  background: rgba(0, 0, 0, 0.4);
-  box-sizing: border-box;
-  transition: 0.5s;
-`;
-
-const InfoTab = styled.div`
-  z-index: 1000;
-  position: absolute;
-  width: 72px;
-  height: 29px;
-  left: 60px;
-  top: -30px;
-  cursor: pointer;
-  background: #f1dbb1;
-  border: 1px solid #ffdc61;
-  box-sizing: border-box;
-  border-radius: 4px 4px 0px 0px;
-`;
-
-const InfoText = styled.p`
-  margin-top: -1px;
-  font-family: Pirata One;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 24px;
-  line-height: 31px;
-  /* identical to box height */
-
-  color: #000000;
-`;
-
-const InfoFeed = styled.div`
-  overflow: auto;
-  height: 235px;
-`;
-
-const WelcomeMessage = styled.h1`
-  font-family: Pirata One;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 24px;
-  line-height: 31px;
-  /* identical to box height */
-
-  color: #ffffff;
-`;
