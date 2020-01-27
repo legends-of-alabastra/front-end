3b30fdd04daad07ec65db21fa6c54a64fef54906
Mychal Hall <45377552+mychal-hall@users.noreply.github.com>
2020-01-27 17:29:14 -0600

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,3 @@
-
-import React from 'react';
-import Login from './components/state/types/Login'
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={() => <Login />} />
-      </Router>
-    </div>
-  );
-
 import React, { Component } from 'react';
 // import axios from 'axios';
 
