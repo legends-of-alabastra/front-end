563ef5f86bc38eb1827d6ea5d8d67dda03096bc2
Miguel Diaz <diaz4674@gmail.com>
2020-01-28 15:14:27 -0800

    Commented out unfinished file

--- front-end/src/components/MainWindow.js
+++ front-end/src/components/MainWindow.js
@@ -9,21 +9,21 @@ import Game from "./game/Game";
 // import UI components
 // import MapWindow from "./MapWindow.js";
 import InfoWindow from "./InfoWindow.js";
-import ChatWindow from "./ChatWindow.js";
+// import ChatWindow from "./ChatWindow.js";
 
 export default class MainWindow extends React.Component {
   render() {
     const main_view = {
-      overflow: 'hidden',
-      width: '100vw',
-      height: '100vh'
-    }
-    
+      overflow: "hidden",
+      width: "100vw",
+      height: "100vh"
+    };
+
     return (
-      <div style = { main_view }>
+      <div style={main_view}>
         <Game />
         <InfoWindow />
-        <ChatWindow />
+        {/* <ChatWindow /> */}
       </div>
     );
   }
